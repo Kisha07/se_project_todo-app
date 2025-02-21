42be652073c2f9eb73995b9225ca30dabf73499e
Nikisha <nmwilliams07@gmail.com>
2025-02-20 21:12:44 -0800

    New update for my project

--- components/Todo.js
+++ components/Todo.js
@@ -1,18 +1,25 @@
 class Todo {
-  constructor(data, selector) {
+  constructor(data, selector, handleCheck, handleDelete) {
     this._data = data;
     this._templateElement = document.querySelector(selector);
+    this._handleCheck = handleCheck;
+    this._handleDelete = handleDelete;
   }
 
   _setEventListeners() {
     this._todoDeleteBtn.addEventListener("click", () => {
+      this._handleDelete(this._completed);
+      //this._remove();
       if (this._todoCheckboxEl.checked) {
-        this._completed = !this._completed;
+        this._completed = true;
+        this._handleCheck(this._completed);
       }
       this._todoElement.remove();
     });
     this._todoCheckboxEl.addEventListener("change", () => {
-      this._data.completed = !this._data.completed;
+      this._completed = this._todoCheckboxEl.checked;
+      this._handleCheck(this._completed);
+      this._data.completed = this._completed;
     });
   }
 
